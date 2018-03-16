f8417f496e94076ba3df7a8c44528e6ad1964efa
Neoscris <greg16@neuf.fr>
2018-03-16 12:01:12 +0100

    Ajout du fichier constantes

---
+++ constantes.py
@@ -0,0 +1,20 @@
+"""Constantes du jeu de Labyrinthe MacGyver"""
+
+#Paramètres de la fenêtre
+nombre_sprite_cote = 15
+taille_sprite = 30
+cote_fenetre = nombre_sprite_cote * taille_sprite
+
+#Personnalisation de la fenêtre
+titre_fenetre = "MacGyver Labyrinthe"
+image_icone = "images/mg_droite.png"
+
+#Listes des images du jeu
+image_accueil = "images/accueil.png"
+image_fond = "images/fond.jpg"
+image_mur1 = "images/mur1.png"
+image_aiguille = "images/aiguille.jpg"
+image_ether = "images/ether.jpeg"
+image_gardien = "images/gardien.png"
+image_tube = "images/tube.jpg"
+image_seringue = "images/seringue.jpg"
